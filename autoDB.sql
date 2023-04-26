ec3ed00666437abf23de1e5813502c93a05330e4
Armøn <122364524+Armooooon@users.noreply.github.com>
2023-04-26 13:21:30 +0200

    Updated da code
    New code, new tables, vely good liceclacker.

--- autoDB.sql
+++ autoDB.sql
@@ -71,3 +71,75 @@ CREATE TABLE if not exists Reparatur (
     FOREIGN KEY (Auto_ID) REFERENCES Auto(autoNr),
     FOREIGN KEY (Mitarbeiter_ID) REFERENCES Mitarbeiter(mitarbeiterNr)
 );
+
+CREATE TABLE if not exists Termine (
+    ID INT NOT NULL AUTO_INCREMENT,
+    kunde_ID INT NOT NULL,
+    auto_ID INT NOT NULL,
+    datum DATE NOT NULL,
+    beschreibung VARCHAR(255) NOT NULL,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (kunde_ID) REFERENCES Kunde(kundenNr),
+    FOREIGN KEY (auto_ID) REFERENCES Auto(autoNr)
+);
+
+CREATE TABLE if not exists Rechnungsposition (
+    ID INT NOT NULL AUTO_INCREMENT,
+    Rechnung_ID INT NOT NULL,
+    Position INT NOT NULL,
+    Beschreibung VARCHAR(255) NOT NULL,
+    Preis DECIMAL(10, 2) NOT NULL,
+    Menge INT NOT NULL,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (Rechnung_ID) REFERENCES Rechnung(ID)
+);
+
+CREATE TABLE if not exists Fahrzeugbestand (
+    autoNr INT(50) NOT NULL AUTO_INCREMENT,
+    marke VARCHAR(50) NOT NULL,
+    modell VARCHAR(50) NOT NULL,
+    jahr INT(20) NOT NULL,
+    kilometerstand INT(20),
+    kaufdatum DATE NOT NULL,
+    kaufpreis DECIMAL(10, 2) NOT NULL,
+    verkaufsstatus VARCHAR(20) NOT NULL DEFAULT 'zum Verkauf',
+    PRIMARY KEY (autoNr)
+);
+
+CREATE TABLE if not exists Ersatzteil (
+    teilNr INT(50) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    beschreibung VARCHAR(255),
+    preis DECIMAL(10, 2) NOT NULL,
+    lagerbestand INT(20),
+    lieferantNr INT(50) NOT NULL,
+    PRIMARY KEY (teilNr),
+    FOREIGN KEY (lieferantNr) REFERENCES Lieferant(lieferantNr)
+);
+
+CREATE TABLE if not exists Reparatur_Ersatzteil (
+    reparaturID INT NOT NULL,
+    ersatzteilID INT NOT NULL,
+    anzahl INT NOT NULL,
+    PRIMARY KEY (reparaturID, ersatzteilID),
+    FOREIGN KEY (reparaturID) REFERENCES Reparatur(ID),
+    FOREIGN KEY (ersatzteilID) REFERENCES Ersatzteil(ersatzteilNr)
+);
+
+CREATE TABLE if not exists Verkauf_Ersatzteil (
+    verkaufID INT NOT NULL,
+    ersatzteilID INT NOT NULL,
+    anzahl INT NOT NULL,
+    PRIMARY KEY (verkaufID, ersatzteilID),
+    FOREIGN KEY (verkaufID) REFERENCES Verkauf(ID),
+    FOREIGN KEY (ersatzteilID) REFERENCES Ersatzteil(ersatzteilNr)
+);
+
+CREATE TABLE if not exists Lieferant (
+    lieferantNr INT(50) AUTO_INCREMENT,
+    name VARCHAR(99) NOT NULL,
+    adresse VARCHAR(99) NOT NULL,
+    telefon VARCHAR(99) NOT NULL,
+    email VARCHAR(20) NOT NULL,
+    PRIMARY KEY (lieferantNr)
+);
