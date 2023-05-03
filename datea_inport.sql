e699aa22d91915636ddc1f1b66fd52215d77606e
Inori <117300320+Inhorni@users.noreply.github.com>
2023-05-03 13:48:38 +0200

    Update datea_inport.sql

--- datea_inport.sql
+++ datea_inport.sql
@@ -24,12 +24,20 @@ VALUES (1, 'BMW', '320i', 2019, 30000),
 (5, 'Ford', 'Fiesta', 2021, 10000);
 
 -- Insert data into Rechnung table
-INSERT INTO Rechnung (Verkauf_ID, Datum, Summe)
-VALUES (1, '2022-04-01', 25000.00),
-(2, '2022-04-02', 20000.00),
-(3, '2022-04-03', 15000.00),
-(4, '2022-04-04', 18000.00),
-(5, '2022-04-05', 12000.00);
+INSERT INTO auto_db.rechnung (Verkauf_ID, Datum, Summe, kunde_kundenNr)
+VALUES (1, '2023-04-25', 123.45, 1);
+
+INSERT INTO auto_db.rechnung (Verkauf_ID, Datum, Summe, kunde_kundenNr)
+VALUES (2, '2023-04-26', 678.90, 2);
+
+INSERT INTO auto_db.rechnung (Verkauf_ID, Datum, Summe, kunde_kundenNr)
+VALUES (3, '2023-04-27', 234.56, 3);
+
+INSERT INTO auto_db.rechnung (Verkauf_ID, Datum, Summe, kunde_kundenNr)
+VALUES (4, '2023-04-28', 789.01, 4);
+
+INSERT INTO auto_db.rechnung (Verkauf_ID, Datum, Summe, kunde_kundenNr)
+VALUES (5, '2023-04-29', 345.67, 5);
 
 INSERT INTO Verkauf (Auto_ID, Kunde_ID, Datum, Preis, Zahlungsmethode, Mitarbeiter_ID, Rechnungs_ID)
 VALUES (1, 2, '2022-03-14', 8000.00, 'Barzahlung', 1, 1),
@@ -44,17 +52,3 @@ VALUES (1, 2, '2022-02-14', 'Ölwechsel', 150.00),
 (3, 1, '2022-04-18', 'Reparatur des Kupplungssystems', 1200.00),
 (4, 2, '2022-05-21', 'Reparatur des Motors', 3500.00),
 (5, 4, '2022-06-14', 'Austausch des Kühlers', 800.00);
-
-INSERT INTO Termine (kunde_ID, auto_ID, datum, beschreibung)
-VALUES (2, 1, '2022-04-01', 'Ölwechsel und Inspektion'),
-(3, 4, '2022-05-15', 'Reparatur der Bremsen'),
-(1, 5, '2022-06-05', 'Inspektion und Reifenwechsel'),
-(4, 2, '2022-07-12', 'Austausch der Batterie'),
-(2, 3, '2022-08-01', 'Inspektion und Ölwechsel');
-
-INSERT INTO Rechnungsposition (Rechnung_ID, Position, Beschreibung, Preis, Menge)
-VALUES (1, 1, 'Gebrauchtwagen', 8000.00, 1),
-(2, 1, 'Finanzierung', 12000.00, 1),
-(3, 1, 'Gebrauchtwagen', 6500.00, 1),
-(4, 1, 'Finanzierung', 15000.00, 1),
-(5, 1, 'Gebrauchtwagen', 9000.00, 1);
